e7920d8a26a7e2e1fb35234deefda7a815f21bdd
Mimosa Poon <mimosapoon0703@gmail.com>
2016-10-23 22:47:50 +0800

    no color change for tile

--- js/components/tiles/TileBase.js
+++ js/components/tiles/TileBase.js
@@ -5,6 +5,7 @@ var TileBase = function(_type, _posX, _posY) {
     this.width = 40;
     this.height = 20;
     this.ctx = GameStores.getCanvasContext();
+    this.isUpdateColor = false;
 };
 
 TileBase.prototype.draw = function() {
@@ -16,65 +17,27 @@ TileBase.prototype.draw = function() {
 TileBase.prototype.drawBlock = function() {
     this.ctx.beginPath();
 
-    if(GameStores.level === 1) {
-        this.ctx.fillStyle="#ff2155";
-    }else if(GameStores.level === 2) {
-        this.ctx.fillStyle="#00ffff";
-    }
-
-     // this.ctx.fillStyle="#ff2155";
+    this.ctx.fillStyle="#ff2155";
     this.ctx.fillRect(this.x, this.y, this.width, 3);
 
-
-    if(GameStores.level === 1) {
-        this.ctx.fillStyle="#ff2155";
-    }else if(GameStores.level === 2) {
-        this.ctx.fillStyle="#00ffff";
-    }
-    // this.ctx.fillStyle="#ff2155";
+    this.ctx.fillStyle="#ff2155";
     this.ctx.fillRect(this.x, this.y + this.height - 3, this.width, 3);
 
-    if(GameStores.level === 1) {
-        this.ctx.fillStyle="#970000";
-    }else if(GameStores.level === 2) {
-        // this.ctx.fillStyle="#00ffff";
-    }
-
-    // this.ctx.fillStyle="#970000";
+    this.ctx.fillStyle="#970000";
     this.ctx.fillRect(this.x, this.y + 3, this.width, 3);
 
-    if(GameStores.level === 1) {
-        this.ctx.fillStyle="#970000";
-    }else if(GameStores.level === 2) {
-        // this.ctx.fillStyle="#00ffff";
-    }
-
-    // this.ctx.fillStyle="#970000";
+    this.ctx.fillStyle="#970000";
     this.ctx.fillRect(this.x, this.y + this.height - 6, this.width, 3);
 
-
-    if(GameStores.level === 1) {
-        this.ctx.strokeStyle="#970000";
-    }else if(GameStores.level === 2) {
-        // this.ctx.strokeStyle="#00ffff";
-    }
-
-    // this.ctx.strokeStyle = "#970000";
+    this.ctx.strokeStyle = "#970000";
     this.ctx.moveTo(this.x,this.y + this.height - 3);
     this.ctx.lineTo(this.x + 10, this.y + 3);
     this.ctx.lineTo(this.x + 20, this.y + this.height - 3);
     this.ctx.lineTo(this.x + 20 - 5, this.y + this.height - 3);
     this.ctx.lineTo(this.x + 10, this.y + 9);
     this.ctx.lineTo(this.x + 5, this.y + this.height - 3);
-    //
 
-    if(GameStores.level === 1) {
-        this.ctx.fillStyle="#970000";
-    }else if(GameStores.level === 2) {
-        // this.ctx.fillStyle="#00ffff";
-    }
-
-    // this.ctx.fillStyle="#970000";
+    this.ctx.fillStyle="#970000";
     this.ctx.moveTo(this.x + 20,this.y + this.height - 3);
     this.ctx.lineTo(this.x + 10 + 20, this.y + 3);
     this.ctx.lineTo(this.x + 20 + 20, this.y + this.height - 3);
